bb08218aef48dbbb564c6c1eae52aaa0fb9b82c3
Karina Kokoshko <karinakokoshko@gmail.com>
2026-02-25 18:23:02 +0200

    Done practice #7

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-
+from app.io.input import input_from_console, read_from_file_builtin, read_from_file_pandas
+from app.io.output import output_to_console, write_to_file_builtin
 
 def main():
-    pass
+    text_from_user = input_from_console()
+    text_from_file = read_from_file_builtin("data.txt")
+    text_from_pandas = read_from_file_pandas("data.csv")
+
+    output_to_console("Результати: ")
+    output_to_console(f"З консолі: {text_from_user}")
+    output_to_console(f"З файлу (builtin): {text_from_file}")
+    output_to_console(f"З файлу (pandas): \n{text_from_pandas}")
+
+    final_result = f"{text_from_user}\n{text_from_file}\n{text_from_pandas}"
+    write_to_file_builtin(final_result, "output_result.txt")
 
 
 if __name__ == '__main__':
